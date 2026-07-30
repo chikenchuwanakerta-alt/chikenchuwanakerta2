c25faf75dc8636ba6d78721f5a1fa1c8cf7e2e73
chikenchuwanakerta-alt <chikenchuwanakerta@gmail.com>
2026-07-30 23:59:57 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'chicken-chu-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo.png',
+  './banner.jpg',
+  './qris.jpg',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
